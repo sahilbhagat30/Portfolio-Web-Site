f6fe6be9cc7a2b118a3754fc9ab7143db3355a67
sahilbhagat30 <your-github-email@example.com>
2024-11-11 16:51:38 -0500

    Profile Picture Update

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => {
                 <div className="left-panel">
                     <div className="profile-section">
                         <div className="profile-image-container">
-                            <img src="/ProfilePicture.png" alt="Sahil Bhagat" />
+                            <img src="src/Images/profile_picture/ProfilePicture.png" alt="Sahil Bhagat" />
                         </div>
                         <h1 className="name">Sahil Bhagat</h1>
                         <p className="tagline">
