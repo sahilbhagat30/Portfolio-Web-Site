6653c4caf420cd42a512453446b5c0a40223e6fd
Sahil Bhagat <49340510+sahilbhagat30@users.noreply.github.com>
2026-08-22 19:11:53 -0400

    fix: correct TypeScript error in FishPond causing build failures

--- src/components/FishPond.tsx
+++ src/components/FishPond.tsx
@@ -696,7 +696,7 @@ export default function FishPond() {
 
     return () => {
       if (rafId.current) cancelAnimationFrame(rafId.current);
-      footer.removeEventListener("click", handleClick);
+      if (footer) footer.removeEventListener("click", handleClick);
     };
   }, []);
 
