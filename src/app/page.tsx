fb9834a936aa9bf38600fbef9fba09a23246edf2
Sahil Bhagat <49340510+sahilbhagat30@users.noreply.github.com>
2026-08-22 19:19:33 -0400

    feat: add infinite scrolling keyword marquee under hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,7 @@ const PersonalProjects = dynamic(() => import("@/components/PersonalProjects"));
 const Photography = dynamic(() => import("@/components/Photography"));
 const TheEnd = dynamic(() => import("@/components/TheEnd"));
 const VinylPlayer = dynamic(() => import("@/components/VinylPlayer"));
+const Marquee = dynamic(() => import("@/components/Marquee"));
 
 export default function Home() {
   const photos = getPhotos();
@@ -32,6 +33,8 @@ export default function Home() {
           <ScrollyCanvas />
         </div>
       </div>
+
+      <Marquee />
 
       {/* About & Skills */}
       <About />
